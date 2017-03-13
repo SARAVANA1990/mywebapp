a9833ecb6469264c1a2ab2180eb026f5e12f1be5
Saravanakumar <Saravanakumar@Saravana>
2017-03-13 22:00:25 +0530

    Local Commit

---
+++ src/main/java/com/mywebapp/controller/AppController.java
@@ -0,0 +1,22 @@
+package com.mywebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+	
+	/*
+	 * This method will provide the medium to add a new employee.
+	 */
+	@RequestMapping(value = { "/new" }, method = RequestMethod.GET)
+	public String newEmployee(ModelMap model) {
+		System.out.println("my controller will call");
+		
+		return "success";
+	}
+
+}
